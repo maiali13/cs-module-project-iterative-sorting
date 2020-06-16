0268a53a470d554e3b4538cfab040f15bf03a3b1
maiali13 <maryamfawaz@gmail.com>
2020-06-16 02:07:50 -0700

    sorting assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,47 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
+    """
+    Start with current index = 0
+    For all indices EXCEPT the last index:
+        a. Loop through elements on right-hand-side of current index and find the smallest element
+        b. Swap the element at current index with the smallest element found in above loop
+    arr = list of sortable elements
+    """
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
-
+        smallest_val = arr[i]
+        for n in range(i, len(arr)):
+            if arr[n] < smallest_val:
+                smallest_index = n
+                smallest_val = arr[n]
 
         # TO-DO: swap
-        # Your code here
-
+        if cur_index != smallest_index:
+            arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+        
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    """
+    Loop through your array
+    Compare each element to its neighbor
+    If elements in wrong position (relative to each other, swap them)
+    If no swaps performed, stop. 
+    Else, go back to the element at index 0 and repeat step 1.
 
+    """
+    for x in range(0, len(arr) - 1):
+        for y in range(0, len(arr) - (x + 1)):
+            if arr[y] > arr[y + 1]:
+                temp = arr[y]
+                arr[y] = arr[y + 1]
+                arr[y + 1] = temp
 
     return arr
 
